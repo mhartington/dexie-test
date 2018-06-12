342850eb4602731ed88c65f885705a8b30d2e267
mhartington <mikehartington@gmail.com>
2018-06-12 19:53:16 -0400

    init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Dexie } from 'dexie';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  db: Dexie;
   constructor(public navCtrl: NavController) {
 
+    this.db = new Dexie('Friends');
+    this.db.version(1).stores({
+      friends: '++id,name,age'
+    });
   }
-
 }
